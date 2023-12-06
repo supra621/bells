31804b2c25f8418df983d6ad1de996493ca88966
Ryan Jackson <supra621@gmail.com>
2023-12-05 20:22:36 -0500

    WORKING STATE
    - Basic JanusGraph integration

--- bells/urls.py
+++ bells/urls.py
@@ -1,8 +1,10 @@
+import gremlin.views
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 import core.views
+import gremlin.views
 
 from . import views
 
@@ -11,6 +13,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls')),
     path('core/', core.views.test_app_view),
+    path('gremlin/', gremlin.views.test_graph),
 ]
 
 if settings.DEBUG:
--- gremlin/views.py
+++ gremlin/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from gremlin_python import statics
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from gremlin_python.process.anonymous_traversal import traversal
+from gremlin_python.process.graph_traversal import __
+from gremlin_python.structure.graph import Graph
+
+
+def test_graph(request):
+    connection = DriverRemoteConnection('ws://janusgraph:8182/gremlin', 'g')
+    g = traversal().withRemote(connection)
+    hercules_age = g.V().has('name', 'hercules').values('age').to_list()
+    return HttpResponse(f'Hercules is {hercules_age} years old.')
